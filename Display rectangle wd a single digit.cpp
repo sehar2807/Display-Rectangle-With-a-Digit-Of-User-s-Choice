55313a84e70237bc29a5c2f16e4b3213c4f06ad1
sehar <57066970+sehar2807@users.noreply.github.com>
2019-12-06 22:58:11 +0500

    Add files via upload

---
+++ Display rectangle wd a single digit.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int n;
+	cout<<"Input The Number : ";
+	cin>>n;
+	cout<<" "<<n<<n<<n<<n<<endl;
+	cout<<" "<<n<<" "<<" "<<n<<endl;
+	cout<<" "<<n<<" "<<" "<<n<<endl;
+	cout<<" "<<n<<" "<<" "<<n<<endl;
+	cout<<" "<<n<<" "<<" "<<n<<endl;
+	cout<<" "<<n<<n<<n<<n<<endl;
+	return 0;
+	}
